9ddc506ccbd2b8f5f5a44093e4c0cb1fa51906ea
Ankit-1204 <ankit2003upadhyay@gmail.com>
2024-05-26 18:31:23 +0530

    fourth commit

---
+++ Frontend/src/theme.js
@@ -0,0 +1,15 @@
+// theme.js
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: "blue.900",
+        color: "white",
+      },
+    },
+  },
+});
+
+export default theme;
