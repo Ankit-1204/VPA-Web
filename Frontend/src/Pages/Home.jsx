7005d80e92255696d223fb57e7454cd257f6a38a
Ankit-1204 <ankit2003upadhyay@gmail.com>
2024-05-26 18:34:26 +0530

    fifth commit

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-/*import React ,{ useState }  from "react";
+import React ,{ useState }  from "react";
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
@@ -12,9 +12,8 @@ function Home() {
         }
     }
     return (
-        <div class="calendar-container">
-        <h1 class="animated">Welcome!!</h1>
-        <h3 class="animated">To your Virtual Personal Assistant</h3>
+        <div>
+        <h1>Hello</h1>
         <FullCalendar
         plugins={[dayGridPlugin,interactionPlugin]}
         initialView="dayGridMonth"
@@ -26,91 +25,4 @@ function Home() {
     )
 }
 
-export default Home;*/
-import React, { useState } from 'react';
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin from '@fullcalendar/interaction';
-import theme from '../theme';
-import {
-  ChakraProvider,
-  Button,
-  Input,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-  useDisclosure
-} from '@chakra-ui/react';
-const Home = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    const [events, setEvents] = useState([]);
-    const [newEventTitle, setNewEventTitle] = useState('');
-    const [selectedDate, setSelectedDate] = useState('');
-  
-    const handleClick = (e) => {
-      setSelectedDate(e.dateStr);
-      onOpen();
-    };
-  
-    const handleAddEvent = () => {
-      if (newEventTitle) {
-        const newEvent = { title: newEventTitle, date: selectedDate };
-        setEvents([...events, newEvent]);
-        setNewEventTitle('');
-        onClose();
-      }
-    };
-  
-    return (
-      <ChakraProvider theme={theme}>
-          <h1 className="animated">Welcome!!</h1>
-          <h3 className="animated">To your Virtual Personal Assistant</h3>
-          <FullCalendar
-            plugins={[dayGridPlugin, interactionPlugin]}
-            initialView="dayGridMonth"
-            events={events}
-            dateClick={handleClick}
-            editable={true}
-            headerToolbar={{
-              left: 'prev,next today',
-              center: 'title',
-              right: 'dayGridMonth,timeGridWeek,timeGridDay'
-            }}
-            buttonText={{
-              today: 'Today',
-              month: 'Month',
-              week: 'Week',
-              day: 'Day'
-            }}
-            eventColor='#378006' // Default event color
-          />
-          <Modal isOpen={isOpen} onClose={onClose}>
-            <ModalOverlay />
-            <ModalContent bg="blue.800" color="white" >
-              <ModalHeader>Add Event</ModalHeader>
-              <ModalCloseButton />
-              <ModalBody>
-                <Input
-                  placeholder="Event Title"
-                  value={newEventTitle}
-                  onChange={(e) => setNewEventTitle(e.target.value)}
-                />
-              </ModalBody>
-              <ModalFooter>
-                <Button colorScheme="blue" mr={3} onClick={handleAddEvent}>
-                  Add Event
-                </Button>
-                <Button variant="ghost" onClick={onClose}>Cancel</Button>
-              </ModalFooter>
-            </ModalContent>
-          </Modal>
-      </ChakraProvider>
-    );
-  };
-  
-  export default Home;
-  
+export default Home;
