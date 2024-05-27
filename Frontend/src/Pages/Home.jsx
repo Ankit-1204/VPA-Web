e99358cca91dff75ccf344d48ce226e3a64eea93
Ankit-1204 <ankit2003upadhyay@gmail.com>
2024-05-28 01:01:42 +0530

    Revert "should not have used firebase "
    This reverts commit 710bd2a5591ac0d27996332e5243b5ffdc1f4f3f.

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,51 +1,21 @@
-/*import React ,{ useState }  from "react";
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin from '@fullcalendar/interaction';
-function Home() {
-    const [events, setEvents] = useState([]);
-    const handleClick=(e)=>{
-        const title=prompt("Enter the event.");
-        if(title){
-            const newEvent = { title: title, date: e.dateStr };
-            setEvents([...events,newEvent]);
-        }
-    }
-    return (
-        <div class="calendar-container">
-        <h1 class="animated">Welcome!!</h1>
-        <h3 class="animated">To your Virtual Personal Assistant</h3>
-        <FullCalendar
-        plugins={[dayGridPlugin,interactionPlugin]}
-        initialView="dayGridMonth"
-        events={events}
-        dateClick={handleClick}
-        editable={true}
-        />
-        </div>
-    )
-}
-
-export default Home;*/
 import React, { useState } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import theme from '../theme';
 import {
-    ChakraProvider,
-    Button,
-    Input,
-    Box,   // Import Flex from Chakra UI
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    useDisclosure
-  } from '@chakra-ui/react';
+  ChakraProvider,
+  Button,
+  Input,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure
+} from '@chakra-ui/react';
 const Home = () => {
     const { isOpen, onOpen, onClose } = useDisclosure();
     const [events, setEvents] = useState([]);
@@ -65,21 +35,11 @@ const Home = () => {
         onClose();
       }
     };
+  
     return (
-      
       <ChakraProvider theme={theme}>
           <h1 className="animated">Welcome!!</h1>
-          <h2 className="animated">To your Virtual Personal Assistant</h2>
-          <div className="calendar-container">
-        <Box mt={4} p={4} bg="blue.800" color="white">
-         <Input
-        placeholder="Input from user"
-        value={newEventTitle}
-        onChange={(e) => setNewEventTitle(e.target.value)}
-        />
-         <Button ml={4} colorScheme="blue" onClick={handleAddEvent}>Enter</Button>
-         </Box>
-
+          <h3 className="animated">To your Virtual Personal Assistant</h3>
           <FullCalendar
             plugins={[dayGridPlugin, interactionPlugin]}
             initialView="dayGridMonth"
@@ -99,10 +59,6 @@ const Home = () => {
             }}
             eventColor='#378006' // Default event color
           />
-          </div>
-
-
-          
           <Modal isOpen={isOpen} onClose={onClose}>
             <ModalOverlay />
             <ModalContent bg="blue.800" color="white" >
@@ -127,5 +83,5 @@ const Home = () => {
     );
   };
   
-export default Home;
+  export default Home;
   
