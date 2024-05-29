bcbece9ffa9d2c5146623f04ff540e8a86acf201
Astha <Astha@astha20226038@mnnit.ac.in>
2024-05-30 01:25:16 +0530

    my commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,17 +2,17 @@ import { useState } from 'react'
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import Home from './Pages/Home'
 import './App.css'
-
+import Admin from './Pages/Admin'
+import Login from './Pages/Login'
+import SignUp from './Pages/SignUp'
 function App() {
 
   return (
-    
     <Router>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
+    <Routes>    
+      <Route path='/' element={<Login/>}/>
     </Routes>
     </Router>
-    
   )
 }
 
