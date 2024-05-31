a0d826c19345d9d7b2741c728fd62ff1a7ceb241
ast003 <rks.astha003@gmail.com>
2024-06-01 00:34:47 +0530

    my cmmit

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,5 +1,3 @@
-
-
 import React, { useState } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
@@ -29,7 +27,8 @@ import {
     Text,
     Heading,
     useColorMode,
-    IconButton
+    IconButton,
+    Container
 } from '@chakra-ui/react';
 import { FiUser, FiMoon, FiSun } from 'react-icons/fi';
 
@@ -62,124 +61,132 @@ const Home = () => {
 
     return (
         <ChakraProvider>
-            <Box width="100%" bg="blue.900" color="white" py={4} px={24}>
-                <Flex justifyContent="space-between" alignItems="center">
-                    <Box>
-                        <Heading size="lg">Welcome</Heading>
-                        <Text fontSize="md">To your Virtual Personal Assistant</Text>
-                    </Box>
-                    <Button leftIcon={<FiUser />} colorScheme="teal" onClick={onDrawerOpen}>
-                        Profile
-                    </Button>
-                </Flex>
-            </Box>
-
-            <Flex justifyContent="center" mt={4} p={4}>
-                <Box width={{ base: '100%', md: '70%', lg: '60%' }} bg={colorMode === 'light' ? 'gray.100' : 'gray.700'} p={4} borderRadius="md" boxShadow="md">
-                    <Flex mb={4}>
-                        <Input
-                            placeholder="Add new event"
-                            value={newEventTitle}
-                            onChange={(e) => setNewEventTitle(e.target.value)}
-                            bg={colorMode === 'light' ? 'white' : 'gray.600'}
-                            color={colorMode === 'light' ? 'black' : 'white'}
-                            borderColor="gray.300"
-                            _hover={{ borderColor: 'gray.400' }}
-                        />
-                        <Button ml={2} colorScheme="teal" onClick={handleAddEvent}>Add Event</Button>
+            <Flex direction="column" minHeight="100vh">
+                <Box width="100%" bg="blue.900" color="white" py={4} px={24}>
+                    <Flex justifyContent="space-between" alignItems="center">
+                        <Box>
+                            <Heading size="lg">Welcome</Heading>
+                            <Text fontSize="md">To your Virtual Personal Assistant</Text>
+                        </Box>
+                        <Button leftIcon={<FiUser />} colorScheme="teal" onClick={onDrawerOpen}>
+                            Profile
+                        </Button>
                     </Flex>
-
-                    <FullCalendar
-                        plugins={[dayGridPlugin, interactionPlugin]}
-                        initialView="dayGridMonth"
-                        events={events}
-                        dateClick={handleClick}
-                        editable={true}
-                        headerToolbar={{
-                            left: 'prev,next today',
-                            center: 'title',
-                            right: 'dayGridMonth,timeGridWeek,timeGridDay'
-                        }}
-                        buttonText={{
-                            today: 'Today',
-                            month: 'Month',
-                            week: 'Week',
-                            day: 'Day'
-                        }}
-                        eventColor='#3182CE'
-                        height="auto"
-                    />
                 </Box>
-            </Flex>
 
-            <Modal isOpen={isOpen} onClose={onClose}>
-                <ModalOverlay />
-                <ModalContent bg={colorMode === 'light' ? 'white' : 'gray.800'} color={colorMode === 'light' ? 'black' : 'white'}>
-                    <ModalHeader>Add Event</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                        <Input
-                            placeholder="Event Title"
-                            value={newEventTitle}
-                            onChange={(e) => setNewEventTitle(e.target.value)}
-                            mb={4}
-                            bg={colorMode === 'light' ? 'gray.100' : 'gray.700'}
-                            color={colorMode === 'light' ? 'black' : 'white'}
-                            borderColor="gray.300"
-                            _hover={{ borderColor: 'gray.400' }}
-                        />
-                        {selectedDateEvents.length > 0 && (
-                            <Box>
-                                <Text mb={2}>Events on {selectedDate}:</Text>
-                                <ul>
-                                    {selectedDateEvents.map((event, index) => (
-                                        <li key={index}>{event.title}</li>
-                                    ))}
-                                </ul>
-                            </Box>
-                        )}
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button colorScheme="teal" mr={3} onClick={handleAddEvent}>
-                            Add Event
-                        </Button>
-                        <Button onClick={onClose}>Cancel</Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
-
-            <Drawer isOpen={isDrawerOpen} placement="right" onClose={onDrawerClose}>
-                <DrawerOverlay />
-                <DrawerContent bg="blue.900" color="white">
-                    <DrawerCloseButton />
-                    <DrawerHeader>Profile</DrawerHeader>
-
-                    <DrawerBody>
-                        <Flex direction="column" alignItems="center" mt={4}>
-                            <Avatar size="xl" name="User Name" src="https://bit.ly/broken-link" />
-                            <Text mt={4} fontSize="xl">User Name</Text>
-                            <Text mt={2} fontSize="md">user@example.com</Text>
+                <Container maxW="container.lg" flex="1" mt={4} mb={16}>
+                    <Box bg={colorMode === 'light' ? 'gray.100' : 'gray.700'} p={4} borderRadius="md" boxShadow="md">
+                        <Flex mb={4}>
+                            <Input
+                                placeholder="Add new event"
+                                value={newEventTitle}
+                                onChange={(e) => setNewEventTitle(e.target.value)}
+                                bg={colorMode === 'light' ? 'white' : 'gray.600'}
+                                color={colorMode === 'light' ? 'black' : 'white'}
+                                borderColor="gray.300"
+                                _hover={{ borderColor: 'gray.400' }}
+                            />
+                            <Button ml={2} colorScheme="teal" onClick={handleAddEvent}>Add Event</Button>
                         </Flex>
-                    </DrawerBody>
 
-                    <DrawerFooter>
-                        <Button colorScheme="teal" mr={3} onClick={onDrawerClose}>
-                            Close
-                        </Button>
-                    </DrawerFooter>
-                </DrawerContent>
-            </Drawer>
+                        <FullCalendar
+                            plugins={[dayGridPlugin, interactionPlugin]}
+                            initialView="dayGridMonth"
+                            events={events}
+                            dateClick={handleClick}
+                            editable={true}
+                            headerToolbar={{
+                                left: 'prev,next today',
+                                center: 'title',
+                                right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                            }}
+                            buttonText={{
+                                today: 'Today',
+                                month: 'Month',
+                                week: 'Week',
+                                day: 'Day'
+                            }}
+                            eventColor='#3182CE'
+                            height="auto"
+                        />
+                    </Box>
+                </Container>
 
-            <IconButton
-                icon={colorMode === 'light' ? <FiMoon /> : <FiSun />}
-                isRound
-                size="lg"
-                position="fixed"
-                bottom={4}
-                right={4}
-                onClick={toggleColorMode}
-                colorScheme="teal"
-            />
+                <Modal isOpen={isOpen} onClose={onClose}>
+                    <ModalOverlay />
+                    <ModalContent bg={colorMode === 'light' ? 'white' : 'gray.800'} color={colorMode === 'light' ? 'black' : 'white'}>
+                        <ModalHeader>Add Event</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody>
+                            <Input
+                                placeholder="Event Title"
+                                value={newEventTitle}
+                                onChange={(e) => setNewEventTitle(e.target.value)}
+                                mb={4}
+                                bg={colorMode === 'light' ? 'gray.100' : 'gray.700'}
+                                color={colorMode === 'light' ? 'black' : 'white'}
+                                borderColor="gray.300"
+                                _hover={{ borderColor: 'gray.400' }}
+                            />
+                            {selectedDateEvents.length > 0 && (
+                                <Box>
+                                    <Text mb={2}>Events on {selectedDate}:</Text>
+                                    <ul>
+                                        {selectedDateEvents.map((event, index) => (
+                                            <li key={index}>{event.title}</li>
+                                        ))}
+                                    </ul>
+                                </Box>
+                            )}
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button colorScheme="teal" mr={3} onClick={handleAddEvent}>
+                                Add Event
+                            </Button>
+                            <Button onClick={onClose}>Cancel</Button>
+                        </ModalFooter>
+                    </ModalContent>
+                </Modal>
+
+                <Drawer isOpen={isDrawerOpen} placement="right" onClose={onDrawerClose}>
+                    <DrawerOverlay />
+                    <DrawerContent bg="blue.900" color="white">
+                        <DrawerCloseButton />
+                        <DrawerHeader>Profile</DrawerHeader>
+
+                        <DrawerBody>
+                            <Flex direction="column" alignItems="center" mt={4}>
+                                <Avatar size="xl" name="User Name" src="https://bit.ly/broken-link" />
+                                <Text mt={4} fontSize="xl">User Name</Text>
+                                <Text mt={2} fontSize="md">user@example.com</Text>
+                            </Flex>
+                        </DrawerBody>
+
+                        <DrawerFooter>
+                            <Button colorScheme="teal" mr={3} onClick={onDrawerClose}>
+                                Close
+                            </Button>
+                        </DrawerFooter>
+                    </DrawerContent>
+                </Drawer>
+
+                <Box as="footer" width="100%" bg="blue.900" color="white" py={4}>
+                    <Flex justifyContent="center">
+                        <Text fontSize="md">© 2024 Onlybank. All rights reserved.</Text>
+                    </Flex>
+                </Box>
+
+                <IconButton
+                    icon={colorMode === 'light' ? <FiMoon /> : <FiSun />}
+                    isRound
+                    size="lg"
+                    position="fixed"
+                    bottom={12}
+                    right={4}
+                    onClick={toggleColorMode}
+                    colorScheme="teal"
+                />
+            </Flex>
         </ChakraProvider>
     );
 };
