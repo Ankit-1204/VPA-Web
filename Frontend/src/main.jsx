03d31064a4866bb8e62b6f66515af6524d3db725
Ankit-1204 <ankit2003upadhyay@gmail.com>
2024-06-16 23:19:44 +0530

    Retrieving files data lost due to merging

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ChakraProvider } from "@chakra-ui/react";
+import { UserContextProvider } from '../context/context.jsx';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
      <ChakraProvider>
+     <UserContextProvider>
+   
       <App />
+
+     </UserContextProvider>
     </ChakraProvider>
   </React.StrictMode>,
 )
-
-
