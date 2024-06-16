485d779ff02938111889b5d7101094bb147c8340
Astha Singh <rks.astha003@gmail.com>
2024-06-16 21:48:48 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,6 @@
 import { useState } from 'react'
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import Home from './Pages/Home'
-
-import Login from './Pages/Login'
-import SignUp from './Pages/SignUp'
-
-
-import Admin from './Pages/Admin'
 import Login from './Pages/Login'
 import SignUp from './Pages/SignUp'
 function App() {
@@ -17,7 +11,6 @@ function App() {
 
       <Route path='/' element={<Login/>}/>
       <Route path='/signup' element={<SignUp/>}/>
-      <Route path='/admin' element={<Admin/>}/>
       <Route path='/home' element={<Home/>}/>
 
     </Routes>
